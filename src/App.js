9e49eed89b816805f82f6e0550d314735995a77c
luanaCristina <luana.cristina@thoughtworks.com>
2022-09-19 00:29:58 -0300

    add e rename files

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ export default class App extends Component {
   render() {
     return (
       <div>
+        <h1>Veja o código no GitHub </h1>
+        <a href='https://github.com/luanaCristina/crud-lu'>Link Github</a>
         <ProductTable />
       </div>
     )
